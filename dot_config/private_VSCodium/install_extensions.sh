4e1a6dc97f8b721439bb800a473c7bdd09cc8021
Tim Voets <tim.voets@dataworkz.nl>
2022-03-21 09:36:30 +0100

    add install extensions script

--- dot_config/private_VSCodium/install_extensions.sh
+++ dot_config/private_VSCodium/install_extensions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while read EXTENSION; do
-  echo_info "Installing VSCode Extension: $EXTENSION"
+  echo "Installing VSCode Extension: $EXTENSION"
   codium --install-extension $EXTENSION
 done < ~/.config/VSCodium/extensions.txt
