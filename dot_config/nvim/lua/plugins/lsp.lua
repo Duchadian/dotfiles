195bbe89934e069790bd9e998b2b610366e95f90
Tim Voets <voets.tim@gmail.com>
2023-07-28 10:04:36 +0200

    switch neovim to lazyvim
    Signed-off-by: Tim Voets <voets.tim@gmail.com>

---
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,62 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    cmd = "Neotree",
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = require("lazyvim.util").get_root() })
+        end,
+        desc = "Explorer NeoTree (root dir)",
+      },
+      {
+        "<leader>fE",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
+      { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+      { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+    },
+    deactivate = function()
+      vim.cmd([[Neotree close]])
+    end,
+    opts = {
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = true,
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            ".DS_Store",
+            "thumbs.db",
+            "node_modules",
+          },
+        },
+      },
+      window = {
+        mappings = {
+          ["<space>"] = "none",
+        },
+      },
+    },
+    {
+      "neovim/nvim-lspconfig",
+      ---@class PluginLspOpts
+      opts = {
+        ---@type lspconfig.options
+        servers = {
+          -- pyright will be automatically installed with mason and loaded with lspconfig
+          pyright = {},
+          rust_analyzer = {},
+          terraformls = {},
+          yamlls = {},
+        },
+      },
+    },
+  },
+}
